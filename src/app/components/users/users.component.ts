5f47c2bc59d51b2b80b569555d2ce3a9efe561e9
Srikanth <srikanth.barenkala@walmartlabs.com>
2020-10-21 01:27:15 +0530

    26 add font-awesome & toggle functionality

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,7 +9,7 @@ import { User } from '../../models/User';
 })
 export class UsersComponent implements OnInit {
   users: User[];
-  showExtended: boolean = false;
+  showExtended: boolean = true;
   loaded: boolean = false;
   enableAdd: boolean = true;
 
@@ -28,7 +28,8 @@ export class UsersComponent implements OnInit {
             state: 'MA'
           },
           isActive: true,
-          registered: new Date('01/02/2018 08:30:00')
+          registered: new Date('01/02/2018 08:30:00'),
+          hide: true
         },
         {
           firstName: 'Kevin',
@@ -40,7 +41,8 @@ export class UsersComponent implements OnInit {
             state: 'MA'
           },
           isActive: false,
-          registered: new Date('03/11/2017 06:20:00')
+          registered: new Date('03/11/2017 06:20:00'),
+          hide: true
         },
         {
           firstName: 'Karen',
@@ -52,7 +54,8 @@ export class UsersComponent implements OnInit {
             state: 'FL'
           },
           isActive: true,
-          registered: new Date('11/02/2016 10:30:00')
+          registered: new Date('11/02/2016 10:30:00'),
+          hide: true
         }
       ];
 
@@ -63,9 +66,8 @@ export class UsersComponent implements OnInit {
     this.users.push(user);
   }
 
-  fireEvent(e) {
-    // console.log('Button Clicked');
-    console.log(e.type);
-  }
+  // toggleHide(user: User) {
+  //   user.hide = !user.hide;
+  // }
   
 }
