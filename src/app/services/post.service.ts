c8fe1c20998d42f6e610d79cf5ff5b91cdb41768
Srikanth <srikanth.barenkala@walmartlabs.com>
2020-10-21 03:51:17 +0530

    40 DELETE req

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -27,4 +27,11 @@ export class PostService {
 
     return this.http.put<Post>(url, post, httpOptions);
   }
+
+  removePost(post: Post | number): Observable<Post> {
+    const id = typeof post === 'number' ? post : post.id;
+    const url = `${this.postsUrl}/${id}`;
+
+    return this.http.delete<Post>(url, httpOptions);
+  }
 }
