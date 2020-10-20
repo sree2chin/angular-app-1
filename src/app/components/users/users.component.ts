5025e56352521c261d3a9e19eb09b52efd760faf
Srikanth <srikanth.barenkala@walmartlabs.com>
2020-10-21 01:05:38 +0530

    22 style binding & ngStyle

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -13,6 +13,7 @@ export class UsersComponent implements OnInit {
   loaded: boolean = false;
   enableAdd: boolean = true;
   currentClasses = {};
+  currentStyles = {};
 
   constructor() { }
 
@@ -65,6 +66,7 @@ export class UsersComponent implements OnInit {
       // });
 
       this.setCurrentClasses();
+      this.setCurrentStyles();
   }
 
   addUser(user: User) {
@@ -75,6 +77,13 @@ export class UsersComponent implements OnInit {
     this.currentClasses = {
       'btn-success': this.enableAdd,
       'big-text': this.showExtended
+    }
+  }
+
+  setCurrentStyles() {
+    this.currentStyles = {
+      'padding-top': this.showExtended ? '0' : '40px',
+      'font-size': this.showExtended ? '' : '40px'
     }
   }
   
