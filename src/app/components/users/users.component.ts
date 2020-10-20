be2343e62229d25891468c6b9a2ed1c044a19036
Srikanth <srikanth.barenkala@walmartlabs.com>
2020-10-21 01:02:22 +0530

    21 class binding & ngClass

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -11,7 +11,8 @@ export class UsersComponent implements OnInit {
   users: User[];
   showExtended: boolean = true;
   loaded: boolean = false;
-  enableAdd: boolean = false;
+  enableAdd: boolean = true;
+  currentClasses = {};
 
   constructor() { }
 
@@ -21,13 +22,14 @@ export class UsersComponent implements OnInit {
         {
           firstName: 'John',
           lastName: 'Doe',
-          age: 30,
+          age: 70,
           address: {
             street: '50 Main st',
             city: 'Boston',
             state: 'MA'
           },
-          image: 'http://lorempixel.com/600/600/people/3'
+          image: 'http://lorempixel.com/600/600/people/3',
+          isActive: true
         },
         {
           firstName: 'Kevin',
@@ -38,7 +40,8 @@ export class UsersComponent implements OnInit {
             city: 'Lynn',
             state: 'MA'
           },
-          image: 'http://lorempixel.com/600/600/people/2'
+          image: 'http://lorempixel.com/600/600/people/2',
+          isActive: false
         },
         {
           firstName: 'Karen',
@@ -49,7 +52,8 @@ export class UsersComponent implements OnInit {
             city: 'Miami',
             state: 'FL'
           },
-          image: 'http://lorempixel.com/600/600/people/1'
+          image: 'http://lorempixel.com/600/600/people/1',
+          isActive: true
         }
       ];
 
@@ -59,10 +63,19 @@ export class UsersComponent implements OnInit {
       //   firstName: 'David',
       //   lastName: 'Jackson'
       // });
+
+      this.setCurrentClasses();
   }
 
   addUser(user: User) {
     this.users.push(user);
+  }
+
+  setCurrentClasses() {
+    this.currentClasses = {
+      'btn-success': this.enableAdd,
+      'big-text': this.showExtended
+    }
   }
   
 }
