9170aa21f6b418990ef5743b8eb1d2420edc4a89
Srikanth <srikanth.barenkala@walmartlabs.com>
2020-10-20 23:22:58 +0530

    add user component with properties & methods

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+  // template: '<h2>John Doe</h2>'
+  // styles: [`
+  //   h2 {
+  //     color: blue
+  //   }
+  // `]
+})
+
+export class UserComponent {
+  // Properties
+  firstName = 'John';
+  lastName = 'Doe';
+  age = 30;
+
+  // Methods
+  constructor() {
+    // this.sayHello();
+    console.log(this.age);
+    this.hasBirthday();
+    console.log(this.age);
+  }
+
+  sayHello() {
+    console.log(`Hello ${this.firstName}`);
+  }
+
+  hasBirthday() {
+    this.age += 1;
+  }
+}
