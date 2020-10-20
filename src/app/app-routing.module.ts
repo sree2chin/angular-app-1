c87a2ea7b84d29a188ca597149b0cbb28df3d057
Srikanth <srikanth.barenkala@walmartlabs.com>
2020-10-21 04:19:40 +0530

    46 not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,11 +9,14 @@ import { PostsComponent } from './components/posts/posts.component';
 
 import { PostComponent } from './components/post/post.component';
 
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
   {path: 'posts', component: PostsComponent},
-  {path: 'post/:id', component: PostComponent}
+  {path: 'post/:id', component: PostComponent},
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
