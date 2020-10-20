48efc24b18ca8c9ff5b32a6d5659f52198f84f85
Srikanth <srikanth.barenkala@walmartlabs.com>
2020-10-21 03:37:01 +0530

    38 input & edit state

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 import { PostService } from '../../services/post.service';
 
 import { Post } from '../../models/Post';
@@ -10,8 +10,9 @@ import { Post } from '../../models/Post';
   styleUrls: ['./post-form.component.scss']
 })
 export class PostFormComponent implements OnInit {
-  post: Post;
   @Output() newPost: EventEmitter<Post> = new EventEmitter();
+  @Input() currentPost: Post;
+  @Input() isEdit: boolean;
 
   constructor(private postService: PostService) { }
 
@@ -26,6 +27,10 @@ export class PostFormComponent implements OnInit {
         this.newPost.emit(post);
       });
     }
+  }
+
+  updatePost() {
+    console.log(123);
   }
 
 }
