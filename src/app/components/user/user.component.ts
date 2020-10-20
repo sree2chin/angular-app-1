e10f3cfec4a49ccfe8cd4fb4f16dbde6c0b3cd3e
Srikanth <srikanth.barenkala@walmartlabs.com>
2020-10-20 23:27:50 +0530

    14 - string interpolation

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -3,34 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css'],
-  // template: '<h2>John Doe</h2>'
-  // styles: [`
-  //   h2 {
-  //     color: blue
-  //   }
-  // `]
+  styleUrls: ['./user.component.css']
 })
 
 export class UserComponent {
   // Properties
   firstName = 'John';
-  lastName = 'Doe';
+  lastName = 'Smith';
   age = 30;
+  address = {
+    street: '50 Main st',
+    city: 'Boston',
+    state: 'MA'
+  };
 
   // Methods
   constructor() {
-    // this.sayHello();
-    console.log(this.age);
-    this.hasBirthday();
-    console.log(this.age);
+    
   }
 
-  sayHello() {
-    console.log(`Hello ${this.firstName}`);
-  }
-
-  hasBirthday() {
-    this.age += 1;
+  showAge() {
+    return this.age + 2;
   }
 }
